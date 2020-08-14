b000ff52a06f93940a2fea47d24914fba385e8ab
harinugraha129 <harin260@gmail.com>
2020-08-14 11:23:21 +0700

    add ignore

--- 1_1_get_started_with_image.py
+++ 1_1_get_started_with_image.py
@@ -1,10 +1,13 @@
-
 import cv2 as cv
 import sys
-img = cv.imread(cv.samples.findFile("data/starry_night.jpg"))
+
+img = cv.imread("data/lena.jpg", 1)
+
 if img is None:
     sys.exit("Could not read the image.")
+
 cv.imshow("Display window", img)
+
 k = cv.waitKey(0)
 if k == ord("s"):
     cv.imwrite("starry_night.png", img)
--- 1_3_Drawing_functions.py
+++ 1_3_Drawing_functions.py
@@ -16,6 +16,8 @@ img = cv2.ellipse(img,(256,256),(100,50),0,0,180,255,-1)
 pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
 pts = pts.reshape((-1,1,2))
 img = cv2.polylines(img,[pts],True,(0,255,255))
+
+cv2.imwrite('result.jpg', img)
 cv2.imshow('image', img)
 
 cv2.waitKey(0)
