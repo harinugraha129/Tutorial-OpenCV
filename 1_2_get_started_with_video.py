aaa3b953ccbaf468b67f6383c9bcd93bd6104c87
harinugraha129 <harin260@gmail.com>
2020-08-15 11:43:35 +0700

    refresh

--- 1_2_get_started_with_video.py
+++ 1_2_get_started_with_video.py
@@ -1,12 +1,13 @@
 import numpy as np
 import cv2 as cv
 
-cap = cv.VideoCapture(0)
-# cap = cv.VideoCapture('data/vtest.avi')
+# cap = cv.VideoCapture(0)
+cap = cv.VideoCapture('data/vtest.avi')
 # cap = cv.VideoCapture('rtsp://192.168.10.20:8080/video/h264')
 
 # Define the codec and create VideoWriter object
 fourcc = cv.VideoWriter_fourcc(*'XVID')
+fourcc = cv.VideoWriter_fourcc(*'MJPG')
 out = cv.VideoWriter('output.avi', fourcc, 20.0, (640,  480))
 
 while cap.isOpened():    
@@ -14,12 +15,25 @@ while cap.isOpened():
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    
-    # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     # frame = cv.flip(frame, 0)
     # write the flipped frame
-    # out.write(frame)
+
+    frame = cv.line(frame, (0,0), (255,255), (147, 96, 44), 10) # 44, 96, 147
+    frame = cv.arrowedLine(frame, (0,255), (255,255), (255, 0, 0), 10)
+    frame = cv.rectangle(frame, (384, 0), (510, 128), (0, 0, 255), 10)
+    frame = cv.circle(frame, (447, 63), 63, (0, 255, 0), -1)
+       
+    font = cv.FONT_HERSHEY_SIMPLEX
+    frame = cv.putText(frame, 'OpenCv', (10, 500), font, 4, (0, 255, 255), 10, cv.LINE_AA)
+    frame = cv.ellipse(frame,(256,256),(100,50),0,0,180,(0,255,0),-1)
+    
+    pts = np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
+    pts = pts.reshape((-1,1,2))
+    frame = cv.polylines(frame,[pts],True,(0,255,255))
+    
+    out.write(frame)
     cv.imshow('frame', frame)
     if cv.waitKey(30) == ord('q'):
         break
@@ -27,3 +41,4 @@ while cap.isOpened():
 cap.release()
 out.release()
 cv.destroyAllWindows()
+
